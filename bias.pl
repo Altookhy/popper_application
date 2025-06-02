567228ccaed2bc590f711968f43d288ab15bd523
Tookhy <osamaaltooky@gmail.com>
2025-06-02 10:49:13 +0200

    objects

--- bias.pl
+++ bias.pl
@@ -18,13 +18,35 @@ direction(cell,(in,in,out)).
 direction(object_available,(in,in)).
 direction(cell_available,(in,in,in)).
 
-% Constants 1
+% Constants
+% 2x2 grid objects
 object(o1).
 object(o2).
 object(o3).
 object(o4).
+% 3x3 grid objects
+object(o5).
+object(o6).
+object(o7).
+object(o8).
+object(o9).
+% 4x4 grid objects
+object(o10).
+object(o11).
+object(o12).
+object(o13).
+object(o14).
+object(o15).
+object(o16).
+
+% 2x2 grid coordinates
 coord(0).
 coord(1).
+% 3x3 grid coordinates
+coord(2).
+% 4x4 grid coordinates
+coord(3).
+
 color(red).
 color(blue).
 color(green).
